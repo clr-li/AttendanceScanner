8b08d82275d31ca7b1a629bd40d492eb698944ad
Glitch (glitch-hello-sqlite) <none>
2022-10-25 23:24:10 +0000

    ⛴🍧 Checkpoint ./package.json:12528664/892 ./server.js:12528664/321 ./public/user.js:12528664/255 ./public/user.html:12528664/1684

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const fs = require("fs");
 
 const https = require('https');
 
+// init sqlite db
+const dbFile = "./.data/businesses.db";
+const exists = fs.existsSync(dbFile);
+let db = null;
+if (!exists) {
+  console.log("no database file found :(");
+}
+else {
+  const sqlite3 = require("sqlite3").verbose();
+  db = new sqlite3.Database(dbFile);
+}
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
