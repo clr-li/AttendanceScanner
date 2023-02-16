5ea270ab79537775485d24e2ed8f5b7cec2ada1a
Glitch (scanner2022) <none>
2023-02-16 21:43:30 +0000

    👾⛳️ Checkpoint ./public/util.js:12528664/594 ./public/admin.html:12528664/2132

--- public/util.js
+++ public/util.js
@@ -41,13 +41,28 @@ export function parseJwt(token) {
     return JSON.parse(ethereumjs.Buffer.Buffer.from(token.split('.')[1], 'base64').toString());
 }
 
-export function LevenshteinDistance(str1, str2) {
+export function calcSimilarity(str1, str2) {
+  return -LevenshteinDistance(str1, str2);
+}
+
+function LevenshteinDistance(str1, str2) {
   let matrix = Array(str1.length + 1).fill().map(() => Array(str2.length + 1).fill(0));
   
-  for (let i = 0; i <= str1.length; i++) {
+  for (let i = 1; i <= str1.length; i++) {
     matrix[i][0] = i;
   }
-  for (let j = 0; j <= str2.length; j++) {
+  for (let j = 1; j <= str2.length; j++) {
     matrix[0][j] = j;
   }
+  
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      let subcost = str1.charAt(i) == str2.charAt(j) ? 0 : 1;
+      matrix[i][j] = Math.min(matrix[i - 1][j] + 1, 
+                              matrix[i][j - 1] + 1, 
+                              matrix[i - 1][j - 1] + subcost);
+    }
+  }
+  
+  return matrix[str1.length][str2.length];
 }
