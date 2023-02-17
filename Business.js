69707ab6ad12e90a078947cdd2046e5a9ee40ee6
Glitch (scanner2022) <none>
2023-02-17 01:23:55 +0000

    🎈⛱ Checkpoint ./Business.js:12528664/39467 ./server.js:12528664/12742

--- Business.js
+++ Business.js
@@ -1,219 +1,274 @@
 // express for routing
 const express = require('express'),
   router = express.Router();
-// braintree for payments
-const braintree = require("braintree");
-const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.MERCHANTID,
-  publicKey: process.env.MERCHANTPUBLIC,
-  privateKey: process.env.MERCHANTPRIVATE
-});
 // database access
 const {db, asyncGet, asyncAll, asyncRun, asyncRunWithID} = require('./Database');
 // user auth
 const handleAuth = require('./Auth').handleAuth;
+// random universal unique ids for joincodes
+const uuid = require('uuid');
 
-// ============================ PAYMENT SETTINGS ============================
-const PLAN_IDS = { // subscription plans
-  STANDARD: "n2wg"
-};
-const PLAN_NAME = new Map(Object.entries(PLAN_IDS).map(keyval => [keyval[1], keyval[0]])); // reverse lookup plan name from plan id
+// ============================ BUSINESS SETTINGS ============================
 
-// ============================ PAYMENT FUNCTIONS ============================
 
-// returns customer_id of user with uid if it exists, otherwise returns false
-async function getCustomerId(uid) {
-  const user = await asyncGet(`SELECT customer_id FROM Users WHERE id = ?`, [uid]);
-  if (!user) return false;
-  return user.customer_id;
+// ============================ BUSINESS FUNCTIONS ============================
+async function createBusiness(uid, name) {
+  const user = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (user.BusinessIDs) return
+  
+  const rand = uuid.v4();
+  const attendanceTableName = "ATT" + rand;
+  const eventTableName = "EVT" + rand;
+  const userTableName = "USR" + rand;
+  await asyncRun(`
+    CREATE TABLE "${userTableName}" (
+        "userid"        TEXT NOT NULL UNIQUE,
+        "role"  TEXT,
+        FOREIGN KEY("userid") REFERENCES "Users"("id"),
+        PRIMARY KEY("userid")
+    );
+  `);
+  await asyncRun(`
+    CREATE TABLE "${eventTableName}" (
+        "id"    INTEGER NOT NULL UNIQUE,
+        "name"  TEXT,
+        "starttimestamp"        TEXT NOT NULL,
+        "userids"       TEXT,
+        "description"   TEXT,
+        "endtimestamp"  TEXT,
+        PRIMARY KEY("id" AUTOINCREMENT)
+    );
+  `);
+  await asyncRun(`
+    CREATE TABLE "${attendanceTableName}" (
+        "eventid"       INTEGER NOT NULL,
+        "userid"        INTEGER NOT NULL,
+        "timestamp"     TEXT NOT NULL,
+        "status"        TEXT NOT NULL,
+        FOREIGN KEY("userid") REFERENCES "${userTableName}"("userid"),
+        FOREIGN KEY("eventid") REFERENCES "${eventTableName}"("id")
+    );
+  `); 
+  const businessID = await asyncRunWithID(`INSERT INTO Businesses (Name, AttendanceTable, usertable, eventtable, roleaccess, joincode) VALUES (?, ?, ?, ?, ?, ?) `, [
+    name, attendanceTableName, userTableName, eventTableName, '{"admin":{"admin":true,"scanner":true}}', uuid.v4()
+  ]);
+  console.log('Created new business with id: ' + businessID);
+  await asyncRun('UPDATE Users SET BusinessIDs = ? WHERE id = ?', [businessID, uid]);
+  await asyncRun(`INSERT INTO "${userTableName}" (userid, role) VALUES (?, ?)`, [uid, 'admin']);
 }
 
-// returns true if the user (specified by uid) subscribes at least once to the planId 
-// throws any error that's not a "notFoundError" (the user hasn't signed up as a customer yet)
-// returns false otherwise
-async function verifySubscription(uid, planId) {
-  const customerId = await getCustomerId(uid);
-  if (!customerId) return false;
-  try {
-    var customer = await gateway.customer.find("" + customerId);
-  } catch (err) {
-    if (err.type === "notFoundError") return false; // this should not be necessary?
-    throw err;
-  }
-  customer.paymentMethods.forEach(paymentMethod => {
-    paymentMethod.subscriptions.forEach(subscription => {
-      if (subscription.status === "Active" 
-          && subscription.planId === planId) {
-          return true;
-      }
-    });
-  });
-  return false;
+async function deleteBusiness(uids, businessID) {
+  await asyncRun(`UPDATE Users SET BusinessIDs = NULL WHERE id IN ('${uids.join("', '")}')`);
+ 
+  const tables = await asyncGet(`SELECT AttendanceTable, usertable, eventtable FROM Businesses WHERE id = ?`, [businessID]);
+  await asyncRun(`DROP TABLE "${tables.AttendanceTable}"`);
+  await asyncRun(`DROP TABLE "${tables.usertable}"`);
+  await asyncRun(`DROP TABLE "${tables.eventtable}"`);
+  await asyncRun(`DELETE FROM Businesses WHERE id = ?`, [businessID]);
+  
+  console.log('Deleted the business with id: ' + businessID);
 }
 
-// this token authorizes the client to access the payment portal and modify customer payment information
-// if the client is already a customer, we give them access to their braintree customer via the stored customer_id
-async function getClientToken(uid) {
-  const customerId = (await asyncGet(`SELECT customer_id FROM Users WHERE id = ?`, [uid])).customer_id;
-  const tokenOptions = {};
-  console.log("CustomerId requested a client token: " + customerId)
-  if (customerId) {
-    tokenOptions.customerId = customerId;
-    tokenOptions.options = {
-      failOnDuplicatePaymentMethod: true,
-      makeDefault: true,
-      verifyCard: true
-    };
-  }
-
-  let res = await gateway.clientToken.generate(tokenOptions);
-  return res.clientToken;
-}
-
-// ============================ PAYMENT ROUTES ============================
-
-// @see getClientToken()
-router.get("/clientToken", async (request, response) => {
+// ============================ ADMIN ROUTES ============================
+router.get("/business", async (request, response) => {
   const uid = await handleAuth(request, response);
   if (!uid) return;
 
-  response.send(await getClientToken(uid));
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  const row = await asyncGet(`SELECT Name FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  response.send(row);
 });
 
-// we get the paymentNonce from the client (a secure way to communicate payment information)
-// and use it to create a customer in the braintree vault
-// or if the client already has a customer, we get that customer using the stored id
-// then we select the default payment method (as set by the client SDK) of the customer 
-// and use the corresponding paymentMethodToken to subscribe to the payment plan
-router.post("/checkout", async (request, response) => {
+router.get("/joincode", async (request, response) => {
   const uid = await handleAuth(request, response);
   if (!uid) return;
 
-  const nonceFromTheClient = request.body.nonce;
-  const deviceData = request.body.deviceData;
-  const businessName = request.body.businessName;
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  const row = await asyncGet(`SELECT id, joincode FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  response.send(row);
+});
 
-  const user = await asyncGet(`SELECT customer_id, name FROM Users WHERE id = ?`, [uid]);
+router.get("/join", async (request, response) => {
+  const uid = await handleAuth(request, response);
+  if (!uid) return;
 
-  let paymentToken;
-  if (user.customer_id) { // customer already exists in braintree vault
-    const result = await gateway.paymentMethod.create({
-      customerId: user.customer_id,
-      paymentMethodNonce: nonceFromTheClient,
-      options: {
-        failOnDuplicatePaymentMethod: true,
-        makeDefault: true,
-        verifyCard: true
-      }
-    });
-    if (!result.success) {
-        // customer validations, payment method validations or card verification is NOT in order
-        response.sendStatus(401);
-        return;
-    }
-    paymentToken = result.paymentMethod.token;
-  } else { // customer doesn't exist, so we use the paymentMethodNonce to create them!
-    const spaceIX = user.name.indexOf(' ');
-    const result = await gateway.customer.create({
-      firstName: user.name.substring(0, spaceIX),
-      lastName: user.name.substring(spaceIX),
-      paymentMethodNonce: nonceFromTheClient,
-      deviceData: deviceData
-    });
-    if (!result.success) {
-        // customer validations, payment method validations or card verification is NOT in order
-        response.sendStatus(401);
-        return;
-    }
-    const customer = result.customer;
-    const customerId = customer.id; // e.g 160923
-    paymentToken = customer.paymentMethods[0].token; // e.g f28wm
+  const businessId = request.query.id;
+  const joincode = request.query.code;
+  // const email = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  const row = await asyncGet(`SELECT joincode, usertable, pendingemails FROM Businesses WHERE id = ?`, [businessId]);
+  if (row.joincode === joincode) {
+      await asyncRun(`INSERT OR IGNORE INTO "${row.usertable}" (userid, role) VALUES (?, 'user')`, [uid]);
+    response.sendStatus(200);
+  } else {
+    response.sendStatus(403);
+  }
+});
 
-    console.log("Created customer with id: " + customerId);
-
-    // save customer id in database so we can find their information in the braintree vault later
-    await asyncRun(`UPDATE Users SET customer_id = ? WHERE id = ?`, [customerId, uid]);
+router.get("/events", async (request, response) => {
+  const uid = await handleAuth(request, response);
+  if (!uid) return;
+  
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (!(await getAccess(id.BusinessIDs, uid, false, true))) {
+    response.sendStatus(403);
+    return;
   }
 
-  const subscriptionResult = await gateway.subscription.create({
-    paymentMethodToken: paymentToken,
-    planId: PLAN_IDS.STANDARD,
-  });
-  if (!subscriptionResult.success) {
-      // customer validations, payment method validations or card verification is NOT in order
-      response.sendStatus(401);
-      return;
-  }
-  console.log("Added subscription via paymentToken: " + paymentToken)
-  await createBusiness(uid, businessName);
+  const table = await asyncGet(`SELECT eventtable FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  const events = await asyncAll(`SELECT id, name, starttimestamp, endtimestamp, userids, description FROM "${table.eventtable}"`);
+  response.status = 200;
+  response.send(events);
+});
 
+router.get("/recordAttendance", async (request, response) => {
+  const uid = await handleAuth(request, response);
+  if (!uid) return;
+  
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (!(await getAccess(id.BusinessIDs, uid, false, true))) {
+    response.sendStatus(403);
+    return;
+  }
+
+  const eventid = request.query.eventid;
+  const userid = request.query.userid;
+  const status = request.query.status;
+  if (typeof status != "string" || typeof userid != "string" || (typeof eventid != "number" && typeof eventid != "string")) throw "Invalid input";
+
+  const table = await asyncGet(`SELECT AttendanceTable FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  await asyncRun(`INSERT INTO "${table.AttendanceTable}" (eventid, userid, timestamp, status) VALUES (?, ?, ?, ?)`, [eventid, userid, Date.now(), status]);
   response.sendStatus(200);
 });
 
-// get's the noncanceled subscriptions
-router.get("/subscriptions", async (request, response) => {
+router.get("/attendancedata", async function(request, response) {
   const uid = await handleAuth(request, response);
   if (!uid) return;
-    
-  const customerId = await getCustomerId(uid);
-  if (!customerId) {
-      response.sendStatus(401);
-      return;
+  
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (!(await getAccess(id.BusinessIDs, uid, true, false))) {
+    response.sendStatus(403);
+    return;
   }
-  const customer = await gateway.customer.find("" + customerId);
-  let subscriptions = [];
+  const eventid = request.query.eventid;
+  const userid = request.query.userid;
+  if (typeof userid != "string") throw new Error("Invalid input");
 
-  customer.paymentMethods.forEach(paymentMethod => {
-    subscriptions = [...subscriptions, ...paymentMethod.subscriptions];
-  });
-
-  subscriptions = subscriptions.filter(subscription => subscription.status != "Canceled");
-
-  subscriptions = subscriptions.map(subscription => {
-    return {
-      plan: PLAN_NAME.get(subscription.planId),
-      nextBillingDate: subscription.nextBillingDate,
-      nextBillAmount: subscription.nextBillAmount,
-      status: subscription.status,
-      id: subscription.id
-    };
-  });
-
-  response.send(subscriptions);
-});
-
-router.get("/cancelSubscription", async (request, response) => {
-  const uid = await handleAuth(request, response);
-  if (!uid) return;
-    
-  const subscriptionId = request.query.id;
-
-  const customerId = await getCustomerId(uid);
-  if (!customerId) {
-      response.sendStatus(401);
-      return;
+  const table = await asyncGet(`SELECT AttendanceTable, usertable FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  console.log(table)
+  let sql = `SELECT Users.firstname, Users.lastname, "${table.AttendanceTable}".* FROM "${table.AttendanceTable}" LEFT JOIN Users ON "${table.AttendanceTable}".userid = Users.id GROUP BY Users.id, "${table.AttendanceTable}".eventid ORDER BY "${table.AttendanceTable}".timestamp DESC`;
+  if (eventid == "*" && userid == "*") {
+    var attendanceinfo = await asyncAll(sql);
+  } else if (eventid == "*") {
+    var attendanceinfo = await asyncAll(sql + "WHERE userid = ?", [userid]);
+  } else if (userid == "*") {
+    var attendanceinfo = await asyncAll(sql + "WHERE eventid = ?", [eventid]);
+  } else {
+    var attendanceinfo = await asyncAll(sql + "WHERE eventid = ? AND userid = ?", [eventid, userid]);
   }
-  const customer = await gateway.customer.find("" + customerId);
-
-  let subscriptions = [];
-  customer.paymentMethods.forEach(paymentMethod => {
-    subscriptions = [...subscriptions, ...paymentMethod.subscriptions];
-  });
-
-  for (let i = 0; i < subscriptions.length; i++) {
-    let subscription = subscriptions[i];
-    if (subscription.id === subscriptionId) {
-      await gateway.subscription.cancel(subscriptionId);
-      response.sendStatus(200);
-      return
+  if (userid == "*") {
+    let userids = new Set();
+    attendanceinfo.forEach((attendanceRecord) => {
+      userids.add(attendanceRecord.userid);
+    });
+    sql = `SELECT Users.firstname, Users.lastname, "${table.usertable}".userid FROM "${table.usertable}" LEFT JOIN Users ON "${table.usertable}".userid = Users.id WHERE `;
+    userids.forEach((uID) => {
+      sql += `"${uID}" != "${table.usertable}".userid AND `;
+    });
+    if (userids.size === 0) {
+      sql = sql.substr(0, sql.length - 7);
+    } else {
+      sql = sql.substr(0, sql.length - 5);
     }
+    attendanceinfo = attendanceinfo.concat(await asyncAll(sql));
   }
-
-  response.sendStatus(403); // subscription is not owned by customer
+  response.send(attendanceinfo);
 });
 
-// ============================ PAYMENT EXPORTS ============================
-exports.paymentRouter = router;
-exports.verifySubscription = verifySubscription;
-exports.PLAN_IDS = PLAN_IDS;
+// router.get("/getBusinesses", async function(request, response) {
+//   let sql = `SELECT`
+// });
+
+router.get("/makeEvent", async function(request, response) {
+  const uid = await handleAuth(request, response);
+  if (!uid) return;
+  
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (!(await getAccess(id.BusinessIDs, uid, true, false))) {
+    response.sendStatus(403);
+    return;
+  }
+
+  const name = request.query.name;
+  const description = request.query.description;
+  const starttimestamp = request.query.starttimestamp;
+  const endtimestamp = request.query.endtimestamp;
+  const userids = request.query.userids;
+  if (typeof name != "string" || typeof description != "string" || typeof userids != "string" || (typeof starttimestamp != "number" && typeof starttimestamp != "string") || (typeof endtimestamp != "number" && typeof endtimestamp != "string")) throw "Invalid input";
+
+  const table = await asyncGet(`SELECT eventtable FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  await asyncRun(`INSERT INTO "${table.eventtable}" (name, starttimestamp, endtimestamp, userids, description) VALUES (?, ?, ?, ?, ?)`,
+                [name, starttimestamp, endtimestamp, userids, description]);
+  const eventid = await asyncGet(`SELECT last_insert_rowid() FROM "${table.eventtable}"`);
+  response.status(200);
+  response.send(eventid);
+});
+
+router.get("/updateevent", async function(request, response) {
+  const uid = await handleAuth(request, response);
+  if (!uid) return;
+  
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (!(await getAccess(id.BusinessIDs, uid, true, false))) {
+    response.sendStatus(403);
+    return;
+  }
+
+  const name = request.query.name;
+  const description = request.query.description;
+  const starttimestamp = request.query.starttimestamp;
+  const endtimestamp = request.query.endtimestamp;
+  const eventid = request.query.eventid;
+  if (typeof name != "string" || typeof description != "string" || (typeof starttimestamp != "number" && typeof starttimestamp != "string") || (typeof endtimestamp != "number" && typeof endtimestamp != "string")) throw "Invalid input";
+
+  const table = await asyncGet(`SELECT eventtable FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  await asyncRun(`UPDATE "${table.eventtable}" SET name = ?, starttimestamp = ?, endtimestamp = ?, description = ? WHERE id = ?`,
+                [name, starttimestamp, endtimestamp, description, eventid]);
+  response.sendStatus(200);
+});
+
+router.get("/deleteevent", async function(request, response) {
+  const uid = await handleAuth(request, response);
+  if (!uid) return;
+  
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (!(await getAccess(id.BusinessIDs, uid, true, false))) {
+    response.sendStatus(403);
+    return;
+  }
+
+  const eventid = request.query.eventid;
+
+  const table = await asyncGet(`SELECT eventtable FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  await asyncRun(`DELETE FROM "${table.eventtable}" WHERE id = ?`, [eventid]);
+  response.sendStatus(200);
+});
+
+router.get("/eventdata", async function(request, response) {
+  const uid = await handleAuth(request, response);
+  if (!uid) return;
+  
+  const id = await asyncGet(`SELECT BusinessIDs FROM Users WHERE id = ?`, [uid]);
+  if (!(await getAccess(id.BusinessIDs, uid, true, false))) {
+    response.sendStatus(403);
+    return;
+  }
+
+  const eventid = request.query.eventid;
+  if (typeof eventid != "string" && typeof eventid != "number") throw "Invalid input";
+
+  const table = await asyncGet(`SELECT eventtable FROM Businesses WHERE id = ?`, [id.BusinessIDs]);
+  const eventinfo = await asyncGet(`SELECT * FROM "${table.eventtable}" WHERE id = ?`, [eventid]);
+  response.send(eventinfo);
+});
+
+// ============================ BUSINESS EXPORTS ============================
