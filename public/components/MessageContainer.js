91780429f0add627e58aa00da793b001e558c595
Alexander Metzger <alexander@metzger.dk>
2023-04-06 18:39:11 -0700

    tweaked MessageContainer styling

--- public/components/MessageContainer.js
+++ public/components/MessageContainer.js
@@ -2,6 +2,7 @@ import { Component } from "../util/Component.js";
 
 /**
  * The MessageContainer component formats a simple message
+ * @attribute color - a CSS color for the message accent
  */
  export class MessageContainer extends Component {
     initialHTML() {
@@ -14,14 +15,14 @@ import { Component } from "../util/Component.js";
             </div>
             <style>
                 #container { background: #ECEFF1; color: rgba(0,0,0,0.87); position: relative; margin: 0; padding: 0; height: 100%; padding-top: 100px; }
-                #message { background: white; max-width: 360px; margin: 0 auto 16px; padding: 32px 24px 16px; border-radius: 3px; }
+                #message { background: white; border-top: 2px solid ${actionColor}; max-width: 360px; margin: 0 auto 16px; padding: 32px 24px 16px; border-radius: 3px; box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24); }
                 ::slotted(h2) { color: ${actionColor}; font-weight: bold; font-size: 16px; margin: 0 0 8px; }
                 ::slotted(h1) { font-size: 22px; font-weight: 300; color: rgba(0,0,0,0.6); margin: 0 0 16px;}
                 ::slotted(p) { line-height: 140%; margin: 16px 0 24px; font-size: 14px; }
-                #message { box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24); }
                 @media (max-width: 600px) {
                     #container, #message { margin-top: 0; background: white; box-shadow: none; }
                     #container { padding-top: 0; border-top: 16px solid ${actionColor}; }
+                    #message { border-top: none; }
                 }
             </style>
         `;
