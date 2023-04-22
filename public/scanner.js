cf394c486d25807cd892ba406a8549be4050a226
Alexander Metzger <alexander@metzger.dk>
2023-04-21 19:52:58 -0700

    fixed scanner file mode

--- public/scanner.js
+++ public/scanner.js
@@ -11,19 +11,24 @@ const eventInfo = await res.json();
 console.log(eventInfo);
 let lastUserId = -1;
 async function onScanSuccess(decodedText, decodedResult) {
-  // Handle on success condition with the decoded text or result.
-  html5QrcodeScanner.pause();
-  console.log(`Scan result: ${decodedText}`, decodedResult);
+    // Handle on success condition with the decoded text or result.
+    let fileMode = false;
+    try {
+        html5QrcodeScanner.pause();
+    } catch {
+        fileMode = true; // scanner is in file mode
+    }
+    console.log(`Scan result: ${decodedText}`, decodedResult);
 
-  if (lastUserId != decodedText) {
-    const status = Date.now() <= parseInt(eventInfo.starttimestamp) * 1000 ? "PRESENT" : "LATE";
-    const res = await GET(`/recordAttendance?eventid=${eventid}&userid=${decodedText}&status=${status}&businessId=${businessId}`);
-    console.log(res.status);
-    html5QrcodeScanner.resume();
-    lastUserId = decodedText;
-  } else {
-    html5QrcodeScanner.resume();
-  }
+    if (lastUserId != decodedText) {
+        const status = Date.now() <= parseInt(eventInfo.starttimestamp) * 1000 ? "PRESENT" : "LATE";
+        const res = await GET(`/recordAttendance?eventid=${eventid}&userid=${decodedText}&status=${status}&businessId=${businessId}`);
+        console.log(res.status);
+        if (!fileMode) html5QrcodeScanner.resume();
+        lastUserId = decodedText;
+    } else {
+        if (!fileMode) html5QrcodeScanner.resume();
+    }
 }
 
 let html5QrcodeScanner = new Html5QrcodeScanner(
