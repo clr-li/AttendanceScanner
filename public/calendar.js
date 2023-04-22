c14fe13de3329cf58894ebc7361b3d08a6d369d7
Alexander Metzger <alexander@metzger.dk>
2023-04-22 11:09:47 -0700

    cleaned up variable use

--- public/calendar.js
+++ public/calendar.js
@@ -1,22 +1,17 @@
-import { GET, POST } from './util/Client.js';
+import { GET } from './util/Client.js';
 
 $("#calendar").evoCalendar();
-let resBusiness = await GET('/businesses');
-let o = await resBusiness.json();
-let businesses = [];
-o.forEach(business => {
-    businesses.push(business);
-});
-let events;
+const resBusiness = await GET('/businesses');
+const businesses = await resBusiness.json();
+
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const colors = ["blue", "red", "purple", "green", "orange", "hotpink", "yellow", "turquoise", "indigo", "maroon", "lime"];
 for (const [i, business] of Object.entries(businesses)) {
-    let resEvent = await GET('/userEvents?businessId=' + business.id);
-    events = await resEvent.json();
+    const resEvent = await GET('/userEvents?businessId=' + business.id);
+    const events = await resEvent.json();
     events.forEach(event => {
         const date = new Date(event.starttimestamp*1000);
         const endDate = new Date(event.endtimestamp*1000);
-        const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
         const month = months[date.getMonth()];
         const day = date.getDate();
         const year = date.getFullYear();
@@ -31,7 +26,7 @@ for (const [i, business] of Object.entries(businesses)) {
             endtime = endDate.getHours() + ":0" + endDate.getMinutes();
         else
             endtime = endDate.getHours() + ":" + endDate.getMinutes();
-        let eventColor = colors[i%colors.length];
+        const eventColor = colors[i%colors.length];
         if (event.status == null && Date.now() > event.endtimestamp * 1000) {
             status = "ABSENT";
         }
