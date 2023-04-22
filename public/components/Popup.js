68a301d83bf7b3480733dfa785acc8bb72c53c5b
Alexander Metzger <alexander@metzger.dk>
2023-04-21 18:05:35 -0700

    corrected z-index on popup

--- public/components/Popup.js
+++ public/components/Popup.js
@@ -1,63 +1,75 @@
 import { Component } from "../util/Component.js";
 
+/**
+ * The Popup component represents a popup message box that blurs the background content.
+ */
 export class Popup extends Component {
     initialHTML() {
         return /* html */`
-        <link rel="stylesheet" href="/styles/reset.css">
-        <div id="popup">
-            <i class="fa fa-circle-xmark" id="cross"></i>
-            <div>
-                <slot></slot>
+            <link rel="stylesheet" href="/styles/reset.css">
+            <div id="popup">
+                <i class="fa fa-circle-xmark" id="cross"></i>
+                <div>
+                    <slot></slot>
+                </div>
             </div>
-        </div>
-        <style>
-            
-            ::slotted(p) {
-                font-size: 1.2rem;
-            }
-            #popup {
-                position: fixed;
-                left: 0;
-                right: 0;
-                top: 0;
-                bottom: 0;
-                margin: auto;
-                min-width: 300px;
-                width: 30%;
-                max-width: var(--max-width);
-                aspect-ratio: 1/1;
-                min-height: 300px;
-                max-height: var(--max-width);
-                background-color: white;
-                text-align: center;
-                padding: 5px;
-                border: 8px solid var(--accent);
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                border-radius: 10px;
-            }
-            :host:before {
-                content: '';
-                backdrop-filter: blur(5px);
-                position: fixed;
-                top: 0; left: 0;
-                width: 100vw;
-                height: 100vh;
-            }
+            <style>
+                :host {
+                    position: absolute;
+                    z-index: 99999;
+                }
 
-            i {
-                position: absolute;
-                right: 5px;
-                top: 5px;
-                font-size: 1.5rem;
-                color: var(--delete-darker);
-                aspect-ratio: 1/1;
-            }
-        </style>
+                ::slotted(p) {
+                    font-size: 1.2rem;
+                }
+
+                #popup {
+                    position: fixed;
+                    left: 0;
+                    right: 0;
+                    top: 0;
+                    bottom: 0;
+                    margin: auto;
+                    min-width: 300px;
+                    width: 30%;
+                    max-width: var(--max-width);
+                    aspect-ratio: 1/1;
+                    min-height: 300px;
+                    max-height: var(--max-width);
+                    background-color: white;
+                    text-align: center;
+                    padding: 5px;
+                    border: 8px solid var(--accent);
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 10px;
+                }
+
+                :host:before {
+                    content: '';
+                    backdrop-filter: blur(5px);
+                    position: fixed;
+                    top: 0; left: 0;
+                    width: 100vw;
+                    height: 100vh;
+                }
+
+                i {
+                    position: absolute;
+                    right: 5px;
+                    top: 5px;
+                    font-size: 1.5rem;
+                    color: var(--delete-darker);
+                    aspect-ratio: 1/1;
+                }
+            </style>
         `;
     }
 
+    /**
+     * Closes this popup and removes it from the DOM.
+     */
     close() {
         if (this.handleCancel) this.handleCancel();
         else this.remove();
@@ -75,12 +87,21 @@ export class Popup extends Component {
         });
     }
 
+    /**
+     * Creates a popup with the given message and appends it to the DOM.
+     * @param {string} message 
+     */
     static alert(message) {
         const popup = document.createElement('pop-up');
         popup.textContent = message;
         document.body.appendChild(popup);
     }
 
+    /**
+     * Creates a confirmation popup with the given message.
+     * @param {string} message the message to prompt the user with.
+     * @returns a Promise that resolves to true if the user clicks 'CONFIRM' and false otherwise.
+     */
     static confirm(message) {
         return new Promise((resolve, reject) => {
             const popup = document.createElement('pop-up');
