e521a64b3b95b5a7869cc20447357bf4c6674e54
Alexander Metzger <alexander@metzger.dk>
2023-05-05 13:55:00 -0700

    smoother homepage reload; fixed bug where home page doesn't show when not logged in

--- public/index.js
+++ public/index.js
@@ -1,10 +1,14 @@
 import { getCurrentUser } from "./util/Auth.js";
-import { QRCode } from "./components/QRCode.js";
 const user = getCurrentUser();
 
 if (user) {
     document.getElementById('getStarted').style.display = 'none';
+    const QRCode = (await import('./components/QRCode.js')).QRCode;
     document.getElementById('navigation').prepend(new QRCode());
 } else {
     document.getElementById('getStarted').style.opacity = '1';
 }
+
+setTimeout(() => {
+    document.body.style.opacity = '1';
+}, 100)
